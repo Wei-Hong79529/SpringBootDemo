8a85a1e9a80051a08f745c6d0128372fa5a44813
JACKHUANG\hyt <jack.huang@hy-tech.com.tw>
2024-01-19 15:24:59 +0800

    新增SpringBootDemo

---
+++ src/main/java/org/example/controller/SimpleController.java
@@ -0,0 +1,19 @@
+package org.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return appName;
+    }
+}
+
