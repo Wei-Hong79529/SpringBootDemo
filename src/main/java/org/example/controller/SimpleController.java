6b18037a489bd9a06cba45c0153bbda5520ef77d
JACKHUANG\hyt <jack.huang@hy-tech.com.tw>
2024-01-23 11:06:39 +0800

    調整Demo

--- src/main/java/org/example/controller/SimpleController.java
+++ src/main/java/org/example/controller/SimpleController.java
@@ -1,19 +1,20 @@
 package org.example.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
 public class SimpleController {
     @Value("${spring.application.name}")
     String appName;
 
-    @GetMapping("/home")
+    @RequestMapping(value = "/home",method = RequestMethod.GET)
     public String homePage(Model model) {
         model.addAttribute("appName", appName);
-        return appName;
+        return "home";
     }
 }
 
