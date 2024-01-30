3424b6558e185354dc26917a898f77187a0242b8
JACKHUANG\hyt <jack.huang@hy-tech.com.tw>
2024-01-30 09:55:36 +0800

    為了在外部TomCat佈署WebApp

--- src/main/java/org/example/SpringBootDemo.java
+++ src/main/java/org/example/SpringBootDemo.java
@@ -2,9 +2,17 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDemo {
+public class SpringBootDemo extends SpringBootServletInitializer {
+    //採用war檔佈署至外部Tomcat，在ServletInitializer 將SpringBootApplication 轉換成WebApps
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootDemo.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemo.class,args);
     }
